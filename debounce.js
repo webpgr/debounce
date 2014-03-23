a73656ec5ec7b1f271d76d7f3de2aac58da83cbc
Falko <falko.krause@staff.hu-berlin.de>
2014-03-23 18:23:45 +0100

    changed behavior to immediate fireing
    fire immediately and if debounce was called several times also at the end when the timer runs out

--- debounce.js
+++ debounce.js
@@ -1,6 +1,6 @@
 var debounce = function(func, wait) {
 	// we need to save these in the closure
-	var timeout, args, context, timestamp;
+	var timeout, args, context, timestamp, call_count = 0;
 
 	return function() {
 
@@ -8,6 +8,12 @@ var debounce = function(func, wait) {
 		context = this;
 		args = [].slice.call(arguments, 0);
 		timestamp = new Date();
+		
+		// immediately fire on the first call
+		if (call_count == 0) {
+			func.apply(context, args);
+		}
+		++call_count;
 
 		// this is where the magic happens
 		var later = function() {
@@ -23,7 +29,10 @@ var debounce = function(func, wait) {
 			// or if not we can null out the timer and run the latest
 			} else {
 				timeout = null;
-				func.apply(context, args);
+				if (call_count > 1) { // only fire if this was not the first call, first call aready fired
+					func.apply(context, args);
+				}
+				call_count = 0; // time is over reset the counter 
 			}
 		};
 
